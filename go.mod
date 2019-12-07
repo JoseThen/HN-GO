48af6de60625b789bbdff965c8a97d445b6dc2ea
JoseThen <thenj@protonmail.com>
2019-12-06 22:43:03 -0500

    Use Flags instead of commands

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/JoseThen/hn
 
 go 1.13
 
-require github.com/JoseThen/HN-GO v0.0.0-20191206142841-999234d74efc // indirect
+require (
+	github.com/JoseThen/HN-GO v0.0.0-20191206142841-999234d74efc // indirect
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	get "github.com/JoseThen/hn/util"
 	"os"
 	"text/tabwriter"
+
+	get "github.com/JoseThen/hn/util"
 )
 
 // Story hn Stories
@@ -19,55 +20,27 @@ func main() {
 	// minwidth, tabwidth, padding, padchar, flags
 	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
 	// SubCommands
-	topCommand := flag.NewFlagSet("top", flag.ExitOnError)
-	newCommand := flag.NewFlagSet("new", flag.ExitOnError)
+	topFlag := flag.Bool("top", true, "Display the top HN posts.")
+	newFlag := flag.Bool("new", true, "Display the newest HN posts.")
+	countFlag := flag.Int("count", 10, "Number of posts to display.")
 
-	// Top subcommand flag pointers
-	var topListNumber int
-	topCommand.IntVar(&topListNumber, "num", 15, "Number of stories to show. Default is 15.")
+	flag.Parse()
 
-	// New subcommand flag pointers
-	// newListNumber := newCommand.Int("num", 15, "Number of stories to show. Default is 15.")
-
-	if len(os.Args) < 2 {
-		fmt.Println("You need to choose a subcommand")
-		os.Exit(1)
-	}
-
-	/*
-		Switch on the subcommand
-		Parse the flags fore the appropriate FlagSet
-		FlagSet.Parse() requires a set of arguments to parse as input
-		os.Args[2:] will be all arguments starting after the subcommand at os.Args[1]
-	*/
-	switch os.Args[1] {
-	case "top":
-		topCommand.Parse(os.Args[2:])
-	case "new":
-		newCommand.Parse(os.Args[2:])
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	// Check which subcommand was Parsed using the FlagSet.Parsed() function. Handle each case accordingly.
-	// FlagSet.Parse() will evaluate to false if no flags were parsed (i.e. the user did not provide any flags)
-	if topCommand.Parsed() {
+	switch {
+	case *topFlag:
 		defer w.Flush()
 		fmt.Fprintf(w, "\n %s\t%s\t", "Title", "Url")
 		fmt.Fprintf(w, "\n %s\t%s\t", "----", "----")
-		var topIds = get.Ids(topListNumber, "top")
+		var topIds = get.Ids(*countFlag, "top")
 		for _, id := range topIds {
 			var results = get.Data(id)
 			fmt.Fprintf(w, "\n %s\t%s\t", results.Title, results.URL)
 		}
-	}
-
-	if newCommand.Parsed() {
+	case *newFlag:
 		defer w.Flush()
 		fmt.Fprintf(w, "\n %s\t%s\t", "Title", "Url")
 		fmt.Fprintf(w, "\n %s\t%s\t", "----", "----")
-		var newIds = get.Ids(topListNumber, "new")
+		var newIds = get.Ids(*countFlag, "new")
 		for _, id := range newIds {
 			var results = get.Data(id)
 			fmt.Fprintf(w, "\n %s\t%s\t", results.Title, results.URL)
