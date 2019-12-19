32f4f90d8149f7225e0e7c753bf236d6efcdb9b4
JoseThen <thenj@protonmail.com>
2019-12-18 20:31:39 -0500

    Update

--- cmd/root.go
+++ cmd/root.go
@@ -2,15 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-  Use:   "hn",
-  Short: "CLI to get HAcker News Posts and Information",
-	Long: `hn is a cli utility to get information on the
-	current posts in Hacker News. It takes advantage of the
-	Hacker News API.`
+	Use:   "hn",
+	Short: "CLI to get HAcker News Posts and Information",
+	Long: `Hn is a cli utility to get information on the current posts
+in Hacker News. It takes advantage of the Hacker News API.`,
 }
 
 // // Execute executes the root command.
@@ -19,18 +20,12 @@ var rootCmd = &cobra.Command{
 // }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
 	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-
-	rootCmd.AddCommand(addCmd)
-	rootCmd.AddCommand(initCmd)
 }
